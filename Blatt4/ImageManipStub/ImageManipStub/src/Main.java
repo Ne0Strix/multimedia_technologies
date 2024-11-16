de42cb287356523d07ee3fab4c3966a34c660730
Armin Lachini <arminla@edu.aau.at>
2024-11-16 10:04:24 +0100

    add Blatt 4

---
+++ Blatt4/ImageManipStub/ImageManipStub/src/Main.java
@@ -0,0 +1,86 @@
+/*
+ * Simple Image Manipulations
+ * Author: Klaus Schoeffmann, 2024
+ */
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    private static void showBufferedImage(BufferedImage img, String title) {
+        JFrame fS = new JFrame(title);
+        ImageLoadingHelper imgComponent = new ImageLoadingHelper(img);
+        fS.add(imgComponent);
+        fS.pack();
+        fS.setVisible(true);
+    }
+
+    private static void saveImageAsFile(BufferedImage img, String name) {
+        //save as file
+        File outputFile = new File(name + ".jpg");
+        try {
+            ImageIO.write(img, "jpg", outputFile);
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please specify image");
+            System.exit(1);
+        }
+
+        //create a window
+        JFrame f = new JFrame(args[0]);
+        addWindowCloseHandler(f);
+
+        //load image and show it in window
+        ImageLoadingHelper origImage = new ImageLoadingHelper(args[0]);
+        f.add(origImage);
+        f.pack();
+        f.setVisible(true);
+
+        //extract pixel data
+        Pixeldata pixeldata = new Pixeldata(origImage.img);
+
+        //-----------Exercise 1-----------
+        pixeldata.print(100,500);
+        pixeldata.print(1000,400);
+        pixeldata.print(1400,1000);
+        pixeldata.count(255, true);
+        pixeldata.count(0, false);
+        //visualize BPs:
+        /*for (int n = 7; n >= 0; n--) {
+            BufferedImage imgBP = pixeldata.getBP(n);
+            showBufferedImage(imgBP, "BP" + n);
+        }*/
+
+        //-----------Exercise 2-----------
+        /*BufferedImage imgVintage = pixeldata.makeVintage(255, 255, 128);
+        showBufferedImage(imgVintage, "Vintage");
+        saveImageAsFile(imgVintage, "Vintage");*/
+
+        //-----------Exercise 5-----------
+        /*BufferedImage imgSaturized = pixeldata.makeSaturized();
+        showBufferedImage(imgSaturized, "Saturized");
+        saveImageAsFile(imgSaturized, "Saturized");*/
+
+        //-----------Exercise 6-----------
+        /*BufferedImage imgSepia = pixeldata.makeSepia(40);
+        showBufferedImage(imgSepia, "Sepia");
+        saveImageAsFile(imgSepia, "Sepia");*/
+    }
+
+    private static void addWindowCloseHandler(JFrame f) {
+        f.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ Blatt4/ImageManipStub/ImageManipStub/src/Pixeldata.java
@@ -0,0 +1,126 @@
+/*
+ * Simple Image Manipulations
+ * Author: Klaus Schoeffmann, 2024
+ */
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+public class Pixeldata {
+    BufferedImage img;
+    int A, R, G, B;
+    // for practical reasons we can use the following members to save intermediate float values for R,G,B in HSVtoRGB
+    // they can be set with helper method setFloatsRGB()
+    private float fR, fG, fB;
+    float H, S, V;
+
+    public Pixeldata(BufferedImage img) {
+        this.img = img;
+        if (img.getType() != BufferedImage.TYPE_3BYTE_BGR)
+            System.out.println("Cannot handle images of type other than TYPE_3BYTE_BGR!");
+    }
+
+    private void extractRGB(int x, int y) {
+        // TODO: Exercise 4.1 - explain the code in this method
+        int pixel = img.getRGB(x, y);
+        A = 255; //no alpha in TYPE_3BYTE_BGR
+        R = (pixel >> 16) & 0xFF;
+        G = (pixel >> 8) & 0xFF;
+        B = pixel & 0xFF;
+    }
+
+    private int toPixel(int r, int g, int b) {
+        return (r & 0xFF) << 16 | (g & 0xFF) << 8 | (b & 0xFF);
+    }
+
+    public void print(int x, int y) {
+        this.extractRGB(x,y);
+        System.out.println("Pixel at (" + x + "," + y + ") = " + R + "/" + G + "/" + B + " (alpha=" + A + ")");
+    }
+
+    public void count(int val, boolean gt) {
+        // TODO: Exercise 4.1 - explain the code in this method
+        int c1=0, c2=0, c3=0;
+
+        for (int y = 0; y < img.getHeight(); y++)
+            for (int x = 0; x < img.getWidth(); x++) {
+                this.extractRGB(x, y);
+                if ((gt ? R >= val : R <= val)) c1++;
+                if ((gt ? G >= val : G <= val)) c2++;
+                if ((gt ? B >= val : B <= val)) c3++;
+            }
+
+        System.out.println("count result for (" + val + "," + gt + "): \t1:" + c1 + "\t2: " + c2 + "\t3: " + c3);
+    }
+
+    public BufferedImage getBP(int n) {
+        // TODO: Exercise 4.1 - explain the code in this method
+
+        BufferedImage imgN = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                this.extractRGB(x, y);
+                int r = (R & (1 << n)) << (7 - n);
+                int g = (G & (1 << n)) << (7 - n);
+                int b = (B & (1 << n)) << (7 - n);
+                imgN.setRGB(x, y, toPixel(r, g, b));
+            }
+        }
+
+        return imgN;
+    }
+
+    public BufferedImage makeVintage(int maxR, int maxG, int maxB) {
+        BufferedImage imgN = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        // TODO: Exercise 4.2: implement a Vintage filter by limiting the RGB values to the specified maxima
+        // return a new BufferedImage, similar to method getBP()
+
+        return imgN;
+    }
+
+    private void RGB2HSV() {
+        //TODO: Exercise 4.3: convert RGB to HSV (save result in H, S, V members).
+    }
+
+    private int clip(float val) {
+        if (val < 0)
+            return 0;
+        else if (val > 255)
+            return 255;
+        else
+            return (int)val;
+    }
+
+    private void setFloatsRGB(float r, float g, float b) {
+        fR = r;
+        fG = g;
+        fB = b;
+    }
+
+    private void HSV2RGB() {
+        //TODO: Exercise 4.4: convert HSV to RGB (save result in R, G, B members).
+        //Note: you might want to use the clip() helper function above
+    }
+
+
+    public BufferedImage makeSaturized() {
+        BufferedImage imgN = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        // TODO: Exercise 4.5 - implement Saturation filter
+
+        return  imgN;
+    }
+
+    public BufferedImage makeSepia(float HtoUse) {
+        BufferedImage imgN = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        // TODO: Exercise 4.6 - implement Sepia filter
+
+        return  imgN;
+    }
+
+
+
+}
